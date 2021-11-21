4e1a660aec5b9a6c21db3a746be5548fd3b020c8
Tony Schaefer <ajs99778@uga.edu>
2021-11-21 16:33:08 -0500

    submitting jobs to queues might work

--- src/managers/__init__.py
+++ src/managers/__init__.py
@@ -1,3 +1,10 @@
 from .filereader_manager import FileReaderManager, FILEREADER_CHANGE, FILEREADER_REMOVED, FILEREADER_ADDED, ADD_FILEREADER
 from .job_manager import JobManager
 from .qm_input_manager import QMInputManager
+from .cluster_template_manager import (
+    ClusterSchedulingSoftwareManager,
+    SlurmDefault,
+    SGEDefault,
+    PBSDefault,
+    LSFDefault,
+)
